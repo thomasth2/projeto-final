904d03f5758dc181fd857ee409d7b9abbcd77a15
thomasth2 <136771667+thomasth2@users.noreply.github.com>
2023-11-18 15:22:27 -0300

    Second Commit

--- main.py
+++ main.py
@@ -24,26 +24,3 @@ print('-' * 50)
 # Ticket médio por produto em cada loja
 ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
 print(ticket_medio)
-
-# Enviar um email com o relatório
-
-def enviar_email():
-    corpo_email = """
-    <p>Prezados,</p>
-    <p>Segue o Relatório de vendas por Loja</p>
-    """
-
-    msg = email.message.Message()
-    msg['Subject'] = "Relatório de Vendas por Loja"
-    msg['From'] = 'thomasgonzaga78@gmail.com'
-    msg['To'] = 'thomasgonzaga78@gmail.com'
-    password = 'lhcokuzyhulhsowh'
-    msg.add_header('Content-Type', 'text/html')
-    msg.set_payload(corpo_email)
-
-    s = smtplib.SMTP('smtp.gmail.com: 587')
-    s.starttls()
-    # Credenciais de Login para enviar o Email
-    s.login(msg['From'], password)
-    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
-    print('Email enviado')
